edc1a48e411e2ebd37f35ddc067a20a514eb60d1
updaun <updauney@daum.net>
2024-08-13 18:37:54 +0900

    chore: joined user list

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, RoomMember
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RoomMember)
+class RoomMemberAdmin(admin.ModelAdmin):
     pass
